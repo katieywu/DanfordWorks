c140c0bac3748bd1a940fdc2be65fc093ab6f050
Noah Shpak <noahshpak@gmail.com>
2019-02-23 13:18:01 -0500

    beginning script that interacts with Strapi

---
+++ src/assets/js/fetch.js
@@ -0,0 +1,5 @@
+import $ from "jquery";
+
+$("#aboutText").text(
+  "Danford Works is currently available at a variety of popup and semi-permanent locations across New York City. Please see below for the list."
+);
